d8d413a0efb8842ea903dbb6df2e406603144d03
Momina Babar <momina.babar@auxcube.com>
2022-06-29 23:42:06 +0500

    fix: prevent extra renders

--- src/useMockProgress.ts
+++ src/useMockProgress.ts
@@ -1,48 +1,45 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef, useMemo } from 'react';
 import useInterval from "./useInterval";
 
 export function useMockProgress() {
   const [progress, setProgress] = useState<number>(0);
   const [timeInterval, setTimeInterval] = useState<number>(500);
   const [manualProgressComplete, setManualProgressComplete] = useState<boolean>(false);
-  const upperLimit = useRef<number>(manualProgressComplete? 98 : 100);
   const increment = useRef<number>(1);
 
+  const upperLimit = useMemo(() => {
+    if (manualProgressComplete) {
+      return 98;
+    } else {
+      return 100;
+    }
+  }, [manualProgressComplete])
+
   const setFullProgress = () => {
-    if(manualProgressComplete){
+    if (manualProgressComplete) {
       setProgress(100);
     }
   };
-
-  const setUpperLimit = () =>{
-    if(manualProgressComplete){
-      upperLimit.current = 98;
-    }else{
-     upperLimit.current = 100;
-    }
-  }
-
-  useEffect(()=>{
-    setUpperLimit();
-  },[manualProgressComplete]);
 
   //modify time interval for increments
   const changeTimeInterval = (timeInterval: number) => {
     setTimeInterval(timeInterval);
   };
 
+  //randomly increment progress in increments between 1 and 10
   const changeIncrement = () => {
-    increment.current = Math.floor(Math.random() * 10) + 1; //randomly increment progress in increments between 1 and 10
+    increment.current = Math.floor(Math.random() * 10) + 1; 
   };
 
   const intervalRef = useInterval(() => {
-    if (progress <= upperLimit.current) {
-      setProgress(progress + increment.current <= upperLimit.current? progress + increment.current : upperLimit.current);
+    if (progress + increment.current <= upperLimit) {
+      setProgress(progress + increment.current);
       changeIncrement();
     } else {
+      setProgress(upperLimit);
       clearInterval(intervalRef.current);
     }
-  },timeInterval);
+  }, timeInterval);
 
   return { progress, setFullProgress, changeTimeInterval, setManualProgressComplete };
 }
